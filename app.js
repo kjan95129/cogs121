0785c5aefc99caf4176b897f189baa34e29de2ab
Kevin <kjan95129@gmail.com>
2018-04-21 21:48:52 -0700

    visual changes to landing page, customize page

--- app.js
+++ app.js
@@ -3,42 +3,31 @@
  * Module dependencies.
  */
 
-var express = require('express');
-var http = require('http');
-var path = require('path');
-var handlebars = require('express3-handlebars')
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const handlebars = require('express3-handlebars')
+const app = express();
 
-var index = require('./routes/index');
-// Example route
-// var user = require('./routes/user');
-var customize = require('./routes/customize');
-var results = require('./routes/results');
-
-var app = express();
+// setters for routes
+const index = require('./routes/index');
+const customize = require('./routes/customize');
+const results = require('./routes/results');
 
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
+
+// creating engine
 app.engine('handlebars', handlebars());
 app.set('view engine', 'handlebars');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(express.cookieParser('IxD secret key'));
-app.use(express.session());
+
+// router
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
-
+// getters for routes
 app.get('/index', index.view);
-// Example route
-// app.get('/users', user.list);
 app.get('/customize', customize.view);
 app.get('/results', results.view);
 
