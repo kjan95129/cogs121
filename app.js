351e82c9c5b24fbf515902d1e290225439cbc5dc
Kevin <kjan95129@gmail.com>
2018-05-02 12:09:55 -0700

    complete database

--- app.js
+++ app.js
@@ -41,6 +41,11 @@ app.use(bodyParser.urlencoded({ extended: true })); // hook up with your app
 
 // GET profile data for a cat
 app.get('/customize/:specs', (req, res) => {
+
+	const max_eyes = 404.73818698;
+	const max_ears = 1002.17563331;
+	const max_nose_mouth = 1026.90992789;
+
 	const specsToLookup = req.params.specs; // matches ':specs' above
 	const specs_arr = specsToLookup.split('+');
 
@@ -48,26 +53,26 @@ app.get('/customize/:specs', (req, res) => {
 	const earsSize = specs_arr[1];
 	const nose_mouthSize = specs_arr[2];
 
-	console.log("eyes1: " + (404.73818698/10)*~~(eyesSize/10));
-	console.log("eyes2: " + (404.73818698/10)*(~~(eyesSize/10)+1));
+	console.log("eyes1: " + (max_eyes/10)*~~(eyesSize/10));
+	console.log("eyes2: " + (max_eyes/10)*(~~(eyesSize/10)+1));
 
-	console.log("ears1: " + (1002.17563331/10)*~~(earsSize/10));
-	console.log("ears2: " + (1002.17563331/10)*(~~(earsSize/10)+1));
+	console.log("ears1: " + (max_ears/10)*~~(earsSize/10));
+	console.log("ears2: " + (max_ears/10)*(~~(earsSize/10)+1));
 
-	console.log("nose_mouth1: " + (1026.90992789/10)*~~(nose_mouthSize/10));
-	console.log("nose_mouth2: " + (1026.90992789/10)*(~~(nose_mouthSize/10)+1));
+	console.log("nose_mouth1: " + (max_nose_mouth/10)*~~(nose_mouthSize/10));
+	console.log("nose_mouth2: " + (max_nose_mouth/10)*(~~(nose_mouthSize/10)+1));
 
 	// db.all() fetches all results from an SQL query into the 'rows' variable:
 	db.all(
 		'SELECT * FROM all_cats WHERE eyes BETWEEN $eyes1 AND $eyes2 AND ears BETWEEN $ears1 AND $ears2 AND nose_mouth BETWEEN $nose_mouth1 AND $nose_mouth2 LIMIT 10',
 		// parameters to SQL query:
 		{
-			$eyes1: (404.73818698/10)*~~(eyesSize/10),
-			$eyes2: (404.73818698/10)*(~~(eyesSize/10)+1),
-			$ears1: (1002.17563331/10)*~~(earsSize/10),
-			$ears2: (1002.17563331/10)*(~~(earsSize/10)+1),
-			$nose_mouth1: (1026.90992789/10)*~~(nose_mouthSize/10),
-			$nose_mouth2: (1026.90992789/10)*(~~(nose_mouthSize/10)+1)
+			$eyes1: (max_eyes/10)*~~(eyesSize/10),
+			$eyes2: (max_eyes/10)*(~~(eyesSize/10)+1),
+			$ears1: (max_ears/10)*~~(earsSize/10),
+			$ears2: (max_ears/10)*(~~(earsSize/10)+1),
+			$nose_mouth1: (max_nose_mouth/10)*~~(nose_mouthSize/10),
+			$nose_mouth2: (max_nose_mouth/10)*(~~(nose_mouthSize/10)+1)
 		},
 		// callback function to run when the query finishes:
 		(err, rows) => {
