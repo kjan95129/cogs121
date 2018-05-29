092cda73fb79fa6216914b69da1cb8d3735f8772
wongkatew <kww006@ucsd.edu>
2018-05-28 22:35:22 -0700

    there is A data visualization

--- public/js/checkshelters.js
+++ public/js/checkshelters.js
@@ -1,2 +1,4 @@
-const cheerio = require('cheerio');
-const $ = cheerio.load('')
+//const cheerio = require('cheerio');
+//const request = require('request');
+// from the san diego humane society page (https://www.sdhumane.org/how-you-can-help/adopt/available-pets/)
+//const $ = cheerio.load();
