fa7798b999aac7488c3a48a092002d1df9ba5a55
unknown <hrc001@ucsd.edu>
2018-06-03 22:52:39 -0700

    user input data visualization

--- public/js/collection.js
+++ public/js/collection.js
@@ -11,7 +11,43 @@ $(document).ready(() => {
 		//database.ref('users/').remove();
 		//console.log("collecting data");
 
-		database.ref('cats/color/').push(color);
+		if (color == "black") {
+			let colorBlack = database.ref('cats/color/black/number');
+			colorBlack.transaction(function(number) {
+				return number+1;
+			});
+		}
+		else if (color == "dark grey") {
+			let colorDarkGrey = database.ref('cats/color/darkgrey/number');
+			colorDarkGrey.transaction(function(number) {
+				return number+1;
+			});
+		}
+		else if (color == "light grey") {
+			let colorLightGrey = database.ref('cats/color/lightgrey/number');
+			colorLightGrey.transaction(function(number) {
+				return number+1;
+			});
+		}
+		else if (color == "white") {
+			let colorWhite = database.ref('cats/color/white/number');
+			colorWhite.transaction(function(number) {
+				return number+1;
+			});
+		}
+		else if (color == "brown") {
+			let colorBrown = database.ref('cats/color/brown/number');
+			colorBrown.transaction(function(number) {
+				return number+1;
+			});
+		}
+		else if (color == "orange") {
+			let colorOrange = database.ref('cats/color/orange/number');
+			colorOrange.transaction(function(number) {
+				return number+1;
+			});
+		}
+
 		database.ref('cats/eyes/').push(eyesApart);
 		database.ref('cats/ears/').push(earsApart);
 		database.ref('cats/eyes to mouth/').push(nose_mouthApart);
